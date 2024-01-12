02f19bac567af6e7366bfd3a986593f5cd063062
EnverJ <Enlem1987@gmail.com>
2024-01-11 20:46:25 -0700

    added negative scenarios

--- test/userPost.js
+++ test/userPost.js
@@ -40,3 +40,50 @@ describe("post call", () => {
       });
   });
 });
+
+describe("Negative tests", () => {
+  it("410 Authentication Failed", async () => {
+    const data = {
+      email: `user-${Math.floor(Math.random() * 99999)}@example.com`,
+      name: "Joe Doe",
+      gender: "male",
+      status: "active",
+    };
+    return (
+      request
+        .post("users")
+        //   .set("Authorization", `${token}`)
+        .send(data)
+        .then((res) => {
+          console.log(res.body);
+
+          console.log(res.body);
+          expect(res.body.code).to.eq(401);
+          expect(res.body.data.message).to.eq("Authentication failed");
+        })
+    );
+  });
+
+  it.only("422 validation Failed", async () => {
+    const data = {
+      email: `user-${Math.floor(Math.random() * 99999)}@example.com`,
+      name: "Joe Doe",
+    };
+    return request
+      .post("users")
+      .set("Authorization", `${token}`)
+      .send(data)
+      .then((res) => {
+        console.log(res.body);
+
+        console.log(res.body);
+        expect(res.body.code).to.eq(422);
+        expect(res.body.data[0].field).to.eq("gender");
+        expect(res.body.data[0].message).to.eq(
+          "can't be blank, can be male of female"
+        );
+        expect(res.body.data[1].field).to.eq("status");
+        expect(res.body.data[1].message).to.eq("can't be blank");
+      });
+  });
+});
